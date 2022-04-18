a79b63f5285aa1f33f09e95b5d9486a6ddaa5c52
tomas <tmaldonadotrs@gmail.com>
2022-04-17 19:39:09 -0700

    Still haven't figured out how to make the results of the round appear through innerHTML after clicking on the button. I'm trying to make a conditional statement inside a function that will output a text depending on the option that the player selected and the option that the cpu selected.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-//o begin with a function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
-
 let moves = ["Rock", "Paper", "Scissor"]; 
-//function computerPlay(){}
-
 
 let cpuMove = moves[Math.floor(Math.random()*moves.length)];
-//console.log(cpuMove);
+
+function computerMove(){
+    return moves[Math.floor(Math.random()*moves.length)]
+}
+
+console.log(cpuMove);
+//alert(computerMove());
 
 
-//Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - 
-//the playerSelection and computerSelection - and then return a string that declares the winner of
- //the round like so: "You Lose! Paper beats Rock"
+
 
 let options = ["Rock", "Paper", "Scissor"];
 
-/**function popNoSelection(){
+function popNoSelection(){
     document.getElementById("human-selection").innerHTML = "No option selected."
 };
-document.getElementById("none").addEventListener("click", popNoSelection);**/
+document.getElementById("none").addEventListener("click", popNoSelection);
 
 
 function popRock(){
@@ -39,7 +39,7 @@ document.getElementById("scissor").addEventListener("click", popScissor);
 
 
 
-function popCpuText(){
+/*function popCpuText(){
     if (cpuMove == "Rock"){
         document.getElementById("cpu-selection").innerHTML = "Rock"
     }
@@ -50,11 +50,11 @@ function popCpuText(){
         document.getElementById("cpu-selection").innerHTML = "Scissor"
     }
 };
-document.getElementById("input").addEventListener("click", popCpuText);
+document.getElementById("input").addEventListener("click", popCpuText);*/
 
 
 
-function storePopCpuTextResults(){
+/*function storePopCpuTextResults(){
     let computerSelection;
     if (cpuMove == "Rock"){
         computerSelection = "Rock"
@@ -67,6 +67,17 @@ function storePopCpuTextResults(){
     }
     return computerSelection;
 }
+console.log(storePopCpuTextResults());*/
+// maybe the problem is that I store the result data in a local variable that other functions cannot access.
+
+/*function buttonClick(){
+    document.getElementById("result").addEventListener("click", showResult)
+}
+
+function showResult(){
+    document.getElementById("resultText").innerHTML = result
+}*/
+
 
 
 function storePopHumanText (){
@@ -80,49 +91,157 @@ function storePopHumanText (){
     else if (document.getElementById("human-selection").addEventListener("click", popScissor)){
         playerSelection = "Scissor"
     }
-    return playerSelection
 }
+let pSR = document.getElementById("human-selection").innerHTML = "Rock";
+let pSP = document.getElementById("human-selection").innerHTML = "Paper";
+let pSS = document.getElementById("human-selection").innerHTML = "Scissor";
+
+storePopHumanText()
 //POTENTIAL PROBLEM IN THIS PREVIOUYS FUNCTION
+
+
 
 let possibleOutcomes = ["Rock beats scissors!", "Paper beats rock!", "Scissor beats paper!", "Stalemate!"];
 
   //  let playerSelection = storePopHumanText();
   //  let computerSelection = storePopCpuTextResults();
+  //document.getElementById("button").addEventListener("click", firstRound());
+       
 
-    function firstRound (x){
-        let result;
-        if (storePopHumanText == "Rock" && storePopCpuTextResults == "Paper") {
-           result = "You lose! " + possibleOutcomes[1];
+    //let combinationOfPossibleOutcomes = [(),(),(),(),(),(),(),(),()]
+        
+   /* let p1 =   "You lose! " + possibleOutcomes[1];
+    let p2 =  possibleOutcomes[3];
+    let p3 = "You win! " + possibleOutcomes[2];
+    let p4 = "You win! " + possibleOutcomes[0];
+    let p5 = "You lose! " + possibleOutcomes[2];
+    let p6 = possibleOutcomes[3];
+    let p7 = possibleOutcomes[3];
+    let p8 = "You win! " + possibleOutcomes[1];
+    let p9 = "You lose! " + possibleOutcomes[0]; */
+
+
+      
+        
+function one(){
+    document.getElementById("round-result").innerHTML = "You lose! " + possibleOutcomes[1];
+}     
+
+function two(){
+    document.getElementById("round-result").innerHTML = possibleOutcomes[3];
+} 
+
+function three(){
+    document.getElementById("round-result").innerHTML = "You win! " + possibleOutcomes[2];
+} 
+
+function four(){
+    document.getElementById("round-result").innerHTML = "You win! " + possibleOutcomes[0];
+} 
+
+function five(){
+    document.getElementById("round-result").innerHTML = "You lose! " + possibleOutcomes[2];
+} 
+
+function six(){
+    document.getElementById("round-result").innerHTML = possibleOutcomes[3];
+} 
+
+function seven(){
+    document.getElementById("round-result").innerHTML = possibleOutcomes[3];
+} 
+
+function eight(){
+    document.getElementById("round-result").innerHTML = "You win! " + possibleOutcomes[1];
+} 
+
+function nine(){
+    document.getElementById("round-result").innerHTML = "You lose! " + possibleOutcomes[0];
+} 
+
+function conditional(pSR, pSP, pSR, cpuMove){
+    let p1 = pSR == "Rock" && cpuMove == "Paper";
+    let p2 = pSP  == "Paper" && cpuMove == "Paper";
+    let p3 = pSS  == "Scissor" && cpuMove == "Paper";
+    let p4 = pSR  == "Rock" && cpuMove == "Scissor";
+    let p5 = pSP  == "Paper" && cpuMove == "Scissor";
+    let p6 = pSS  == "Scissor" && cpuMove == "Scissor"
+    let p7 = pSR  == "Rock" && cpuMove == "Rock";
+    let p8 = pSP  == "Paper" && cpuMove == "Rock";
+    let p9 = pSS  == "Scissor" && cpuMove == "Rock";
+
+        if (p1 == true){
+             one()
         }
-        else if (storePopHumanText == "Paper" && storePopCpuTextResults == "Paper"){
-            result =  possibleOutcomes[3];
+        else if (p2 == true){
+            two()
         }
-        else if (storePopHumanText == "Scissor" && storePopCpuTextResults == "Paper" ){
-            result =  ("You win! " + possibleOutcomes[2]);
+        else if (p3 == true ){
+            three()
         }
-        else if (storePopHumanText == "Rock" && storePopCpuTextResults == "Scissor") {
-            result =  ("You win! " + possibleOutcomes[0]);
+        else if (p4 == true) {
+            four()
         }
-        else if (storePopHumanText == "Paper" && storePopCpuTextResults == "Scissor"){
-            result =  ("You lose! " + possibleOutcomes[2]);
+        else if (p5 == true){
+            five()
         }
-        else if (storePopHumanText == "Scissor" && storePopCpuTextResults == "Scissor" ){
-            result =  possibleOutcomes[3];
-            return result
+        else if (p6 == true){
+            six()
         }
-        else if (storePopHumanText == "Rock" && storePopCpuTextResults == "Rock") {
-            result =  possibleOutcomes[3];
+        else if (p7 == true) {
+            seven()
         }
-        else if (storePopHumanText == "Paper" && storePopCpuTextResults == "Rock"){
-            result =  ("You win! " + possibleOutcomes[1]);
+        else if (p8 == true){
+            eight()
         }
-        else if (storePopHumanText == "Scissor" && storePopCpuTextResults == "Rock" ){
-            result =  ("You lose! " + possibleOutcomes[0]);
-        }   
-        return displayResultsAfterInputs(result);
-         
-    }
-  
+        else if (p9 == true){
+            nine()
+        }
+       /* else{
+            result = "persist, don't give up."
+        }*/
+        //alert(result); 
+}
+    
+   // conditional();
+
+    document.getElementById("form").addEventListener("click", computerMove);
+
+    document.getElementById("button").addEventListener("click", conditional);
+        
+    /*function initRound(){
+    document.getElementById("round-result").innerHTML = conditional();
+    }*/
+
+        //document.getElementById("result").addEventListener("click", cpuMove);
+      
+//            document.getElementById("result")
+        
+       /* function showResult(){
+            firstRound()
+        }*/
+
+
+
+
+    /*function firstRoundConditional(){
+    if (document.getElementById("rock").addEventListener("click", popRock) == true){
+        firstRound();
+        }
+    else if(document.getElementById("paper").addEventListener("click", popPaper) == true){
+        firstRound();
+        }
+    else if(document.getElementById("scissor").addEventListener("click", popScissor) == true){
+        firstRound();
+        }        
+    }*/
+
+    //firstRoundConditional();
+
+  //      document.getElementById("input").addEventListener("click", popCpuText) == true){
+
+
+ //       storePopCpuTextResults == "Rock" || storePopCpuTextResults == "Paper" || storePopCpuTextResults == "Scissor" && storePopHumanText == "Rock" || storePopHumanText == "Paper" || storePopHumanText == "Scissor"){
 
    // document.addEventListener("input", displayResultsAfterInputs);
 
@@ -131,11 +250,23 @@ let possibleOutcomes = ["Rock beats scissors!", "Paper beats rock!", "Scissor be
         document.getElementById("rock", "paper", "scissor").addEventListener("click", trigger);
     } */
 
-    /**function trigger (){
-        document.getElementById("input").addEventListener("click", popCpuText);
-    } */
 
-//round1Result();    
+   
+
+   // trigger();
+//    showResult();
+
+
+    //trigger();
+
+
+/**const objRoundResult = {
+    triggerFunction: trigger(),
+    showResult: showResult()
+}*/
+
+//document.get
+    //round1Result();    
        // document.getElementById("round-result").innerHTML = firstRound(playerSelection, computerSelection)
 
 
@@ -152,4 +283,4 @@ function loopRound(){
   //  console.log(firstRound(playerSelection, computerSelection));
     }
 }
-loopRound();
+//loopRound();
